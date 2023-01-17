84ae2b011e7d0119542d53531a4608e79cec1458
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-01-17 14:55:40 +0100

    Add files via upload

---
+++ AngleBisectorEquation/anglebisectortheorem.py
@@ -0,0 +1,11 @@
+#let's assume DAB and DAC angle are congruent
+#therefore AD is the bisector of CAB
+# Theorem of angle bisector is AB/BD=AC/CD
+
+lenab=4,9
+lenbd=2,6
+lenac=6,5
+
+lencd=(lenac*lenbd)/lenab
+
+print("the lenght of CD is: ",lencd)
