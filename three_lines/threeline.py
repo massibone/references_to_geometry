135dc8a8980d811c59da17b3ed7539263f2f0a32
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-02-02 11:21:44 +0100

    Add files via upload

---
+++ three_lines/threeline.py
@@ -0,0 +1,11 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# evenly sampled time at 200ms intervals
+t = np.arange(0., 5., 0.2)
+
+# red dashes, blue squares and green line
+plt.plot(t, t, 'r--', t, t**2, 'bs', t, t**3, 'g')
+plt.show()
+
